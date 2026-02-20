08a7bb08594c39b8e39dfe77af245cbbfdf60d6b
AurelienDelcey <aurelien.delcey@gmail.com>
2026-02-20 14:03:40 +0100

    feat(application): add anomaly dto and dto mapper class

--- src/main/java/application/AnomalyDto.java
+++ src/main/java/application/AnomalyDto.java
@@ -1,5 +1,23 @@
 package application;
 
-public class AnomalyDto {
+import java.time.Instant;
+
+import domain.anomaly.AnomalyState;
+import domain.valueobject.QualityDecision;
+
+public record AnomalyDto(String ID,
+		String correctiveActionId,
+		String provingDocumentId,
+		QualityDecision qualityDecision,
+		AnomalyState anomalyState,
+		String description,
+		String createBy,
+		Instant createAt,
+		String correctedBy,
+		Instant correctedAt,
+		String resolvedBy,
+		Instant resolvedAt,
+		String archivedBy,
+		Instant archivedAt) {
 
 }
--- src/main/java/application/AnomalyDtoMapper.java
+++ src/main/java/application/AnomalyDtoMapper.java
@@ -1,5 +1,45 @@
 package application;
 
-public class AnomalyDtoMapper {
+import java.time.Instant;
 
+import domain.anomaly.Anomaly;
+import domain.anomaly.AnomalyState;
+import domain.traceability.EventTrace;
+import domain.traceability.Traceability;
+import domain.valueobject.QualityDecision;
+
+public final class AnomalyDtoMapper {
+	
+	
+	
+	private AnomalyDtoMapper() {
+	}
+
+	public static AnomalyDto mapToDto(Anomaly anomaly) {
+		Traceability traceability = anomaly.getTraceability();
+		
+		String id = anomaly.getId().toString();
+		String correctiveActionId = anomaly.getCorrectiveAction() == null ? null : anomaly.getCorrectiveAction().documentId();
+		String provingDocumentId = anomaly.getProvingDocument() == null ? null : anomaly.getProvingDocument().documentId();
+		QualityDecision qualityDecision = anomaly.getQualityDecision();
+		AnomalyState anomalyState = anomaly.getAnomalyState();
+		String description = anomaly.getDescription().description();
+		String createBy = actorOrNull(traceability.getCreation());
+		Instant createAt = instantOrNull(traceability.getCreation());
+		String correctedBy = actorOrNull(traceability.getToCorrected());
+		Instant correctedAt = instantOrNull(traceability.getToCorrected());
+		String resolvedBy = actorOrNull(traceability.getToResolved());
+		Instant resolvedAt = instantOrNull(traceability.getToResolved());
+		String archivedBy = actorOrNull(traceability.getToArchived());
+		Instant archivedAt = instantOrNull(traceability.getToArchived());
+		return new AnomalyDto(id, correctiveActionId, provingDocumentId, qualityDecision, anomalyState, description, createBy, createAt, correctedBy, correctedAt, resolvedBy, resolvedAt, archivedBy, archivedAt);
+	}
+	
+	private static String actorOrNull(EventTrace trace) {
+	    return trace == null ? null : trace.actorId();
+	}
+
+	private static Instant instantOrNull(EventTrace trace) {
+	    return trace == null ? null : trace.instant();
+	}
 }
