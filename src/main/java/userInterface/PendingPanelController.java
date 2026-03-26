30bc9d906b3551403a5db1b600c8da26ed71c930
AurelienDelcey <aurelien.delcey@gmail.com>
2026-03-26 15:12:08 +0100

    feat(ui): initialize pending panel form with anomaly data

--- src/main/java/userInterface/PendingPanelController.java
+++ src/main/java/userInterface/PendingPanelController.java
@@ -120,6 +120,86 @@ public class PendingPanelController {
     	this.commandService = commandService;
     	this.updateCallback = updateCallback;
     	
+    	setupBoxes();
+    	setupTextFields();
+    	applyFilterOnTextField(impactedQuantityTextField);
+    	applyFilterOnTextField(productionOrderTextField);
+    	preselectComboBoxes();
+    	preselectQualityDecision();
     }
+    
+    private void setupBoxes() {
+		EnumSet<Sector> sectors = EnumSet.allOf(Sector.class);
+		List<String> list = sectors.stream()
+			    .map(Enum::name)
+			    .toList();
+		sectorCombo.setItems(FXCollections.observableArrayList(list));
+		
+		EnumSet<Machine> machine = EnumSet.allOf(Machine.class);
+		List<String> listMachine = machine.stream()
+			    .map(Enum::name)
+			    .toList();
+		machineCombo.setItems(FXCollections.observableArrayList(listMachine));
+	}
+    
+    private void setupTextFields() {
+    	if(anomalyProperty.get() == null) {
+    		return;
+    	}
+    	String correctiveAction = anomalyProperty.get().correctiveActionId();
+    	if(correctiveAction != null && !(correctiveAction.isBlank())) {
+    		correctiveActionTextField.setText(correctiveAction);
+    	}
+    	
+    	String description = anomalyProperty.get().description();
+    	if(description != null && !(description.isBlank())) {
+    		descriptionTextArea.setText(description);
+    	}
 
+    	Integer ProductionOrder = anomalyProperty.get().productionOrder();
+    	if(ProductionOrder != null && ProductionOrder > 0) {
+    		productionOrderTextField.setText(ProductionOrder.toString());
+    	}
+    	
+    	Integer impactedQuantity = anomalyProperty.get().impactedQuantity();
+    	if(impactedQuantity != null && impactedQuantity >= 0) {
+    		impactedQuantityTextField.setText(impactedQuantity.toString());
+    	}
+    }
+    
+    private void applyFilterOnTextField(TextField text) {
+		TextFormatter<String> format = new TextFormatter<>(i->{
+			if(i.getControlNewText().matches("[0-9]*")) {
+				return i;
+			}else {
+				return null;
+			}
+		});
+		text.setTextFormatter(format);
+	}
+    
+    private void preselectComboBoxes() {
+    	if(anomalyProperty.get() == null) {
+    		return;
+    	}
+    	
+    	if(anomalyProperty.get().machine() != null) {
+    		machineCombo.setValue(anomalyProperty.get().machine());   		
+    	}
+    	
+    	if(anomalyProperty.get().sector() != null) {
+    		sectorCombo.setValue(anomalyProperty.get().sector());    		
+    	}
+    }
+    
+    private void preselectQualityDecision() {
+    	if(anomalyProperty.get() != null && anomalyProperty.get().qualityDecision() != null) {
+    		switch(anomalyProperty.get().qualityDecision()) {
+    		case "REPAIR" -> repairButton.setSelected(true);
+    		case "SCRAP" -> scrapButton.setSelected(true);
+    		case "N/A" -> naButton.setSelected(true);
+    		default ->{}
+    		}
+    	}
+    }
 }
