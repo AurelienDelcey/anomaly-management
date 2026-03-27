f7d79f2367242ad351e066478497974bde341e61
AurelienDelcey <aurelien.delcey@gmail.com>
2026-03-27 16:30:56 +0100

    feat(ui): add CorrectedPanelController skelton

--- src/main/java/userInterface/CorrectedPanelController.java
+++ src/main/java/userInterface/CorrectedPanelController.java
@@ -1,5 +1,33 @@
 package userInterface;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
 public class CorrectedPanelController {
+
+    @FXML private Label correctiveActionLabel;
+    @FXML private TextArea descriptionTextArea;
+    @FXML private TextField evidenceTextField;
+    @FXML private Label impactedQuantityLabel;
+    @FXML private Label machineLabel;
+    @FXML private Label productionOrderLabel;
+    @FXML private Label qualityDecisionLabel;
+    @FXML private Button resolveAnomalyButton;
+    @FXML private Label sectorLabel;
+    @FXML private Button validEvidenceButton;
+
+    @FXML
+    void onClickResolveAnomaly(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onClickValidEvidence(ActionEvent event) {
+
+    }
 
 }
